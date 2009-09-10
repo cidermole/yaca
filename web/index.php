227c56d90edde40482f889c930a2fc2b8c29c058
md2k7 <david@madlnet.net>
2009-09-10 11:38:16 +0200

    web: reduced sleep

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@ $common = new Common("192.168.1.2", 1111);
 $led = new Led($common, array('canid_read' => 3, 'canid_write' => 2));
 
 $common->handleRequests();
-usleep(500 * 1000);
+usleep(100 * 1000);
 
 //////////////////////////////////////////////////////////////////
 
