bd519c06174da2ef947f875a88c203fab9e2ab56
md2k7 <david@madlnet.net>
2009-12-13 19:52:13 +0100

    main-power: I and U monitoring

--- src/embedded/main-power/MainPower.C
+++ src/embedded/main-power/MainPower.C
@@ -12,18 +12,46 @@
 
 #define OCR_VAL 200
 
+#define ADC_U_OUT 0
+#define ADC_I_IN  1
+#define ADC_U_IN  2
+
+#define ADMUX_REF ((1 << REFS1) | (1 << REFS0)) // AREF = 2.56 V
+
+// TODO: soft-start (with reduced OCR_VAL) -- blew 2 A fuse without warning :-)
+
 // PC5: red led
 // PC4: green led
 // PB0: charger: low-freq PWM
 // PB1: PWM for step-up regulator (OC1A)
 
-volatile uint8_t ac_power = 0, dc_maybe = 0;
+volatile uint8_t ac_power = 0, dc_maybe = 0, status_update = 0;
 volatile uint16_t ac_status_scaler = 0;
 volatile uint8_t charge_pwm_scaler = 0;
 
+
+void delay_ms(uint16_t t) {
+	uint16_t i;
+	for(i = 0; i < t; i++)
+		_delay_ms(1);
+}
+
+uint16_t adc_convert(uint8_t channel) {
+	ADMUX = (channel & 0x07) | ADMUX_REF; // select channel (+ keep reference)
+	ADCSRA |= (1 << ADSC) | (1 << ADIF); // start conversion, clear int flag
+	while(!(ADCSRA & (1 << ADIF))); // wait for conversion
+
+	return ADCW;
+}
+
+void adc_init() {
+	ADMUX = (1 << REFS1) | (1 << REFS0);
+	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1); // enable ADC, prescaler = 64 (8 MHz / 64 = 125 kHz, 13 cycles per conversion ~ 100 us conversion)
+	delay_ms(100); // wait for ADC to warm up
+	adc_convert(0); // dummy conversion
+}
+
 int main() {
-	uint8_t old_ac = 0;
-	
 	DDRC |= (1 << PC5) | (1 << PC4);
 	DDRB |= (1 << PB1) | (1 << PB0);
 	ACSR = (1 << ACBG); // Analog Comp: + is bandgap ref. = 1,23 V
@@ -35,9 +63,8 @@ int main() {
 	OCR1A = OCR_VAL;
 
 	sei();
-
-	yc_prepare(21);
-    yc_send(MainPower, HelloWorld(eeprom_read_byte(((uint8_t *) 4))));
+	
+	adc_init();
 
 	while(1) {
 		if(ac_power) {
@@ -49,10 +76,10 @@ int main() {
 			PORTC |= (1 << PC5); // red on
 		}
 		
-		if(ac_power != old_ac) {
+		if(status_update) { // every 100 ms
 			yc_prepare(22);
-			yc_send(MainPower, PowerStatus(ac_power));
-			old_ac = ac_power;
+			yc_send(MainPower, PowerStatus(ac_power, adc_convert(ADC_U_IN), adc_convert(ADC_I_IN)));
+			status_update = 0;
 		}
 		
 		yc_dispatch_auto();
@@ -66,6 +93,9 @@ void enter_bootloader_hook() {
 	TCCR1B = 0;
 	TCNT1 = 0;
 	TIMSK = 0;
+	DDRB &= ~(1 << PB0); // stop charging
+	PORTC &= ~(1 << PC4); // green off
+	PORTC &= ~(1 << PC5); // red off
 	yc_bld_reset();
 }
 
@@ -79,13 +109,14 @@ ISR(TIMER1_OVF_vect) {
 	
 	if(++charge_pwm_scaler == 0) { // 256 -> every 8.2 ms (122 Hz)
 		if(ac_power)
-			DDRB ^= (1 << PB0);
+			DDRB |= (1 << PB0); // was ^= for PWM
 	}
 	
 	if(++ac_status_scaler == 3125) { // every 100 ms, update AC status
 		ac_status_scaler = 0;
 		ac_power = !dc_maybe;
 		dc_maybe = 0;
+		status_update = 1;
 	}
 }
 
--- src/embedded/main-power/MainPower.h
+++ src/embedded/main-power/MainPower.h
@@ -2,10 +2,6 @@
 #include <inttypes.h>
 
 namespace MainPower {
-/*	void HDM(SetStatus(uint8_t a, uint8_t b, uint16_t c, uint8_t d));
-	void HDM(LightStatus(uint8_t a));
-	void HDS(Status(uint8_t a));*/
-	void HDS(HelloWorld(uint8_t a));
-	void HDS(PowerStatus(uint8_t ac));
+	void HDS(PowerStatus(uint8_t ac_power, uint16_t adc_voltage, uint16_t adc_current));
 }
 
