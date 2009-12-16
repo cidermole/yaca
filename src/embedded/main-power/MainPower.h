a2234595623e7480d93013f6f1fefa428b315e3a
md2k7 <david@madlnet.net>
2009-12-16 22:49:38 +0100

    main-power: fixed compile error

--- src/embedded/main-power/MainPower.h
+++ src/embedded/main-power/MainPower.h
@@ -4,6 +4,7 @@
 #define YC_EE_POWERSTATUS_ID YE(508) // int32
 
 namespace MainPower {
-	void HDR(PowerStatus(uint8_t ac_power, uint16_t adc_voltage, uint16_t adc_current));
+	void HDS(PowerStatus(uint8_t ac_power, uint16_t adc_voltage, uint16_t adc_current));
+	void HDR(PowerStatus());
 }
 
