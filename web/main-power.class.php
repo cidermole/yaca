34c104778a4c9a4bd72fe8669a429f5912422580
md2k7 <david@madlnet.net>
2009-12-16 22:36:17 +0100

    main-power: minor changes for CAN-ID from EEPROM, web: main-power.class.php with status, U and I display. untested.

---
+++ web/main-power.class.php
@@ -0,0 +1,38 @@
+<?php
+
+class MainPower extends Plugin {
+	function MainPower(&$common, $config) {
+		$this->name = "MainPower";
+		Plugin::Plugin($common, $config);
+	}
+	
+	function getStatus($msg) {
+		$states = array('Idle', 'Boost', 'Charge', 'Charge (topping)');
+		return (isset($states[$msg->data[0]]) ? $states[$msg->data[0]] : 'Undefined');
+	}
+	
+	function getVoltage($msg) {
+		$raw_data = $msg->data[1] * 0x100 + $msg->data[2];
+		return number_format($raw_data * 110.08 / 10240, 2, ',');
+	}
+	
+	function getCurrent($msg) {
+		$raw_data = $msg->data[3] * 0x100 + $msg->data[4];
+		return number_format($raw_data * 30.72 / 6545.408, 3, ',');
+	}
+	
+	function handleRequest() {
+	}
+	
+	function render() {
+		$msg = new Message($this->common);
+		if(!$msg->request($this->config['canid_powerstatus'])) {
+			echo "{MainPower: request " . $this->config['canid_powerstatus'] . " failed}";
+			return false;
+		}
+		echo "{MainPower: status = " . $this->getStatus($msg) . ", U = " . $this->getVoltage($msg) . ", I = " . $this->getCurrent($msg) . " }";
+		return true;
+	}
+}
+
+?>
