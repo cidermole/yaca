246599cf0a75949a222d9c7bea0ded4c1f7b7046
md2k7 <david@madlnet.net>
2009-12-26 22:59:14 +0100

    web: ids

--- web/index.php
+++ web/index.php
@@ -6,8 +6,8 @@ require_once('main-power.class.php');
 
 $common = new Common("192.168.1.2", 1111);
 
-$led = new Led($common, array('canid_read' => 200, 'canid_write' => 400));
-$mp = new MainPower($common, array('canid_powerstatus' => 22, 'canid_charge' => 23));
+$led = new Led($common, array('canid_read' => 200, 'canid_write' => 1));
+$mp = new MainPower($common, array('canid_powerstatus' => 400, 'canid_charge' => 2));
 
 $common->handleRequests();
 usleep(100 * 1000);
