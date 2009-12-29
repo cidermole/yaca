9ec6605585f62a82a91cc37a08d1ec937229e450
md2k7 <david@madlnet.net>
2009-12-29 19:12:13 +0100

    yaca-cached: network.cpp hand 'merge'

--- src/x86/yaca-cached/network.h
+++ src/x86/yaca-cached/network.h
@@ -11,9 +11,16 @@ struct Message {
         uint8_t data[8];
 } __attribute__((__packed__));
 
+
 int connect_socket(const char *host, int port);
 
 int read_message(int sock, struct Message *buffer);
+
+char *target_flash_page(const char *buffer, int page_size, int tid, int sock);
+
+uint16_t crc16_update(uint16_t crc, uint8_t c);
+
+void target_eeprom_write(uint32_t id, int sock, uint16_t addr, uint8_t data);
 
 int create_host(int port);
 
