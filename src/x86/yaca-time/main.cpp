8a5b32fb38392ccf0c09b32599c60bd7a8a1e3ce
md2k7 <david@madlnet.net>
2009-12-29 18:15:57 +0100

    yaca-time: read buffer overflow prevention

--- src/x86/yaca-time/main.cpp
+++ src/x86/yaca-time/main.cpp
@@ -45,6 +45,7 @@ int main(int argc, char **argv) {
 		t.tv_sec++;
 		tm = localtime(&t.tv_sec);
 		write_message(sock, /* TODO */ 401, 7, tm->tm_hour, tm->tm_min, tm->tm_sec, tm->tm_year - 100, tm->tm_mon + 1, tm->tm_mday, tm->tm_isdst);
+		read(sock, buf, sizeof(buf));
 	}
 
 #ifdef _WIN32
