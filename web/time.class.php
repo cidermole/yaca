483fae32863ff3420ea8bd7002af198640b599d7
md2k7 <david@madlnet.net>
2009-12-29 18:04:14 +0100

    web: time.class.php

---
+++ web/time.class.php
@@ -0,0 +1,27 @@
+<?php
+
+class Time extends Plugin {
+	function Time(&$common, $config) {
+		$this->name = "Time";
+		Plugin::Plugin($common, $config);
+	}
+	
+	function handleRequest() {
+	}
+	
+	function nf($number) {
+		return sprintf("%02d", $number);
+	}
+	
+	function render() {
+		$msg = new Message($this->common);
+		if(!$msg->request($this->config['canid_time'])) {
+			echo "{Time: request " . $this->config['canid_time'] . " failed}";
+			return false;
+		}
+		echo $this->nf($msg->data[0]) . ":" . $this->nf($msg->data[1]) . ":" . $this->nf($msg->data[2]) . " " . $this->nf($msg->data[5]) . "." . $this->nf($msg->data[4]) . "." . ($msg->data[3] + 2000);
+		return true;
+	}
+}
+
+?>
