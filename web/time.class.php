065aa07ccc3ed76aeedaf82508ac4e41ce2fa336
md2k7 <david@madlnet.net>
2009-12-30 17:12:57 +0100

    web/time: changed year to 16-bit

--- web/time.class.php
+++ web/time.class.php
@@ -19,7 +19,7 @@ class Time extends Plugin {
 			echo "{Time: request " . $this->config['canid_time'] . " failed}";
 			return false;
 		}
-		echo $this->nf($msg->data[0]) . ":" . $this->nf($msg->data[1]) . ":" . $this->nf($msg->data[2]) . " " . $this->nf($msg->data[5]) . "." . $this->nf($msg->data[4]) . "." . ($msg->data[3] + 2000) . " (" . (($msg->data[6] & (1 << 1)) ? 'backup' : 'NTP') . " time source, " . (($msg->data[6] & (1 << 0)) ? 'CEST' : 'CET') . ")";
+		echo $this->nf($msg->data[0]) . ":" . $this->nf($msg->data[1]) . ":" . $this->nf($msg->data[2]) . " " . $this->nf($msg->data[6]) . "." . $this->nf($msg->data[5]) . "." . ($msg->data[3] * 0x100 + $msg->data[4]) . " (" . (($msg->data[7] & (1 << 1)) ? 'backup' : 'NTP') . " time source, " . (($msg->data[7] & (1 << 0)) ? 'CEST' : 'CET') . ")";
 		return true;
 	}
 }
