0d37600fe0db5851757942b48eea7ec470c959e2
David Madl <freeware@abanbytes.eu>
2010-07-14 22:22:54 +0200

    web: implemented pool control; removed unused stuff

---
+++ web/pool-control.class.php
@@ -0,0 +1,36 @@
+<?php
+
+class PoolControl extends Plugin {
+	function PoolControl(&$common, $config) {
+		$this->name = "PoolControl";
+		Plugin::Plugin($common, $config);
+	}
+	
+	function getPh($msg) {
+		$raw_data = $msg->data[0] * 0x100 + $msg->data[1];
+		return number_format($raw_data / 100.0, 2, ',', '.');
+	}
+	
+	function getTemp($msg) {
+		$raw_data = $msg->data[0] * 0x100 + $msg->data[1];
+		return number_format($raw_data / 10.0, 1, ',', '.');
+	}
+	
+	function render() {
+		$msg_ph = new Message($this->common);
+		if(!$msg_ph->request($this->config['canid_phstatus'])) {
+			echo "{PoolControl: request " . $this->config['canid_phstatus'] . " failed}";
+			return false;
+		}
+		$msg_te = new Message($this->common);
+		if(!$msg_te->request($this->config['canid_tempstatus'])) {
+			echo "{PoolControl: request " . $this->config['canid_tempstatus'] . " failed}";
+			return false;
+		}
+
+		echo "{PoolControl: pH = " . $this->getPh($msg_ph) . ", T = " . $this->getTemp($msg_te) . " }";
+		return true;
+	}
+}
+
+?>
