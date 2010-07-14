6fd67c605216780ee20eb2a148e81b6ffd95240c
David Madl <freeware@abanbytes.eu>
2010-07-14 22:24:51 +0200

    web: pool deg celsius

--- web/pool-control.class.php
+++ web/pool-control.class.php
@@ -28,7 +28,7 @@ class PoolControl extends Plugin {
 			return false;
 		}
 
-		echo "{PoolControl: pH = " . $this->getPh($msg_ph) . ", T = " . $this->getTemp($msg_te) . " }";
+		echo "{PoolControl: pH = " . $this->getPh($msg_ph) . ", T = " . $this->getTemp($msg_te) . " °C }";
 		return true;
 	}
 }
