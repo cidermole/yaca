1f823f164ec7724a8c13278979172a76643d100c
David Madl <freeware@abanbytes.eu>
2010-07-16 23:04:41 +0200

    testcode: slimmed down for can-id flashing on new nodes

--- src/embedded/testcode/Switcha.C
+++ src/embedded/testcode/Switcha.C
@@ -1,25 +1,2 @@
 #include "Switcha.h"
-#include "RSwitcha.h"
-#include <yaca.h>
-#include <avr/io.h>
-
-void init() {
-	DDRD |= (1 << PD4);
-}
-
-void DM(SetLed(uint8_t a)) {
-	// handle it
-	if(a)
-		PORTD |= (1 << PD4);
-	else
-		PORTD &= ~(1 << PD4);
-	
-	yc_prepare_ee(YC_EE_LEDSTATUS_ID);
-	yc_send(Switcha, LedStatus(a));
-}
-
-void DR(LedStatus()) {
-	yc_prepare_ee(YC_EE_LEDSTATUS_ID);
-	yc_send(Switcha, LedStatus((PORTD & (1 << PD4)) ? 1 : 0));
-}
 
--- src/embedded/testcode/Switcha.h
+++ src/embedded/testcode/Switcha.h
@@ -1,11 +1,6 @@
 #include "Messages.h"
 #include <inttypes.h>
 
-#define YC_EE_LEDSTATUS_ID YE(508) // int32
-
 namespace Switcha {
-	void HDM(SetLed(uint8_t a));
-	void HDS(LedStatus(uint8_t a));
-	void HDR(LedStatus());
 }
 
