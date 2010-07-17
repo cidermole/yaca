c844fe206560674581e9c786c59fc330091fff32
David Madl <freeware@abanbytes.eu>
2010-07-17 21:54:59 +0200

    yaca-ppp: first draft

--- src/x86/yaca-ppp/main.cpp
+++ src/x86/yaca-ppp/main.cpp
@@ -13,14 +13,18 @@
 
 
 int main(int argc, char **argv) {
-	int sock = 0, p2c[2], c2p[2], pppd_argc = 1;
-	char config_file[1024];
+	int sock = 0, p2c[2], c2p[2], pppd_argc = 1, max, len;
+	char config_file[1024], buf[sizeof(Message) * 20], *pbuf;
 	char *pppd_args[100], *p, *start, ppp_name[] = "pppd"; // _params
+	Message *mp, msg;
 	pid_t child;
+	fd_set fds;
 
 	init_yaca_path();
 	sprintf(config_file, "%s/src/x86/yaca-ppp/conf/yaca-ppp.conf", yaca_path);
 	load_conf(config_file);
+	msg.id = conf.tcp_out_id;
+	msg.rtr = 0;
 
 	/* load pppd args (split on ' ') */
 	pppd_args[0] = ppp_name;
@@ -56,6 +60,44 @@ int main(int argc, char **argv) {
 	close(c2p[1]);
 	if((sock = connect_socket(conf.server, conf.port)) == -1)
 		return 1;
+
+	while(1) {
+		FD_ZERO(&fds);
+		FD_SET(sock, &fds);
+		FD_SET(c2p[0], &fds);
+		max = sock > c2p[0] ? sock : c2p[0];
+		select(max + 1, &fds, NULL, NULL, NULL);
+
+		if(FD_ISSET(sock, &fds)) {
+			len = read(sock, buf, sizeof(buf));
+			pbuf = buf;
+			while(len > 0) {
+				if(len < sizeof(Message)) {
+					printf("len < sizeof(Message)\n");
+					continue;
+				}
+				pbuf += sizeof(Message);
+				len -= sizeof(Message);
+				mp = (Message *) pbuf;
+				if(mp->id == config.tcp_in_id) {
+					write(p2c[1], mp->data, mp->length);
+				}
+			}
+		} else if(FD_ISSET(c2p[0], &fds)) {
+			len = read(sock, buf, sizeof(buf));
+			pbuf = buf;
+			while(len > 0) {
+				msg.info = 0;
+				msg.length = (len > 8) ? 8 : len;
+				memcpy(msg.data, pbuf, msg.length);
+				write(sock, &msg, sizeof(Message));
+				pbuf += msg.length;
+				len -= msg.length;
+			}
+		} else {
+			printf("select() returned empty... hmmm?\n");
+		}
+	}
 
 #ifdef _WIN32
 	closesocket(sock);
