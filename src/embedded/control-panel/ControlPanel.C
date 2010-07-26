fbf2bae55bfc0190e0516d88f9c7386006ed3ef0
David Madl <freeware@abanbytes.eu>
2010-07-26 20:56:50 +0200

    control-panel: added temp sensor, major restructuring - mainloop content put into a function

--- src/embedded/control-panel/ControlPanel.C
+++ src/embedded/control-panel/ControlPanel.C
@@ -9,10 +9,15 @@
 #include "uart.h"
 #include "../bootloader/msgdefs.h"
 #include "../bootloader/eeprom.h"
+#include "one-wire.h"
 
-uint32_t tcp_in_id, tid;
+#define DS18B20 // using DS18B20 with more resolution than DS18S20
+
+uint32_t tcp_in_id, tcp_out_id, temp_id, tid;
 volatile uint8_t _timer;
-uint8_t _count = 0;
+volatile uint16_t _conv_timer = 1001;
+uint8_t _count = 0, pending = 0;
+Message msg_in, msg_out;
 
 void msg_bld_check(Message *msg) {
 	if(msg->id == tid && msg->data[0] == TID_BLD_ENTER) {
@@ -24,14 +29,110 @@ void msg_bld_check(Message *msg) {
 	yc_dispatch_auto(); // we won't get here. needed for linking of libyaca and consequently the INT0 ISR
 }
 
-int main() {
-	Message msg_in, msg_out;
-	uint8_t i, pending = 0;
+void check_and_receive() {
+	uint8_t i;
 	int c;
+
+	if(yc_poll_receive()) {
+		yc_receive(&msg_in);
+		msg_bld_check(&msg_in);
+		if(msg_in.id == tcp_in_id) {
+			for(i = 0; i < msg_in.length; i++)
+				uart_putc(msg_in.data[i]);
+		}
+
+		// much time has passed... don't busy-wait
+		if(pending)
+			pending = 2;
+	}
+
+	// if we didn't receive something in the meantime, let some time pass
+	if(pending == 1)
+		_delay_us(100);
+
+	if(pending) {
+		if(yc_poll_transmit(&msg_out) == SUCCESS)
+			pending = 0;
+		else
+			continue;
+	}
+
+	msg_out.length = 0;
+	msg_out.info = 0;
+	msg_out.id = tcp_out_id;
+
+	if(fifo2_read.count < 8 && fifo2_read.count > 0) {
+		if(fifo2_read.count > _count) {
+			_count = fifo2_read.count;
+			_timer = 0;
+		}
+		if(_timer < 50) // ms timeout for aggregating messages
+			continue;
+	}
+
+	while((c = uart_getc_nowait()) != -1 && msg_out.length < 8)
+		msg_out.data[msg_out.length++] = c;
+	if(msg_out.length) {
+		if(yc_transmit(&msg_out) == PENDING)
+			pending = 1;
+	}
+}
+
+int16_t measure_temp() {
+	uint8_t data[9], i;
+	int16_t temp_value;
+
+	ow_check(); // ow_check() will not return 0 atm
+/*	if(!ow_check()) {
+		loopdelay_ms(800);
+		return;
+	}*/
+
+	cli();
+	if(_conv_time > 1000) { // start conversion
+		_conv_time = 0;
+		sei();
+		ow_write(OW_SKIP_ROM);
+		ow_write(OW_CONVERT_T, OW_PULL);
+		return INT_MIN;
+	} else if(_conv_time > 800) {
+		_conv_time = 1001;
+		sei();
+	} else {
+		sei();
+		return INT_MIN;
+	}
+
+	ow_release();
+	ow_check(); // same as above
+/*	if(!ow_check())
+		return;*/
+
+	ow_write(OW_SKIP_ROM);
+	ow_write(OW_READ_SCRATCHPAD);
+
+	for(i = 0; i < 9; i++)
+		data[i] = ow_read();
+
+	temp_value = data[0] | (((int16_t) data[1]) << 8);
+#ifdef DS18B20
+	temp_value = (temp_value * 10) / 16;
+#else
+	temp_value *= 5; // 0.5 °C steps
+	// TODO: exact temp measurement with remainder
+#endif
+
+	return temp_value;
+}
+
+int main() {
+	int16_t temp, _temp;
+	uint8_t temp_pending = 0;
 
 	uart_init((uint16_t) (((F_CPU / (16.0 * BAUDRATE) - 1) * 2 + 1) / 2));
 	eeprom_read_block(&tcp_in_id, YC_EE_TCP_IN_ID, sizeof(tcp_in_id));
-	eeprom_read_block(&msg_out.id, YC_EE_TCP_OUT_ID, sizeof(msg_out.id));
+	eeprom_read_block(&tcp_out_id, YC_EE_TCP_OUT_ID, sizeof(tcp_out_id));
+	eeprom_read_block(&temp_id, YC_EE_TEMPSTATUS_ID, sizeof(temp_id));
 	eeprom_read_block(&tid, EE_TEMPID, sizeof(tid));
 	msg_out.rtr = 0;
 
@@ -44,45 +145,18 @@ int main() {
 	sei();
 
 	while(1) {
-		if(yc_poll_receive()) {
-			yc_receive(&msg_in);
-			msg_bld_check(&msg_in);
-			if(msg_in.id == tcp_in_id) {
-				for(i = 0; i < msg_in.length; i++)
-					uart_putc(msg_in.data[i]);
-			}
-
-			// much time has passed... don't busy-wait
-			if(pending)
-				pending = 2;
+		check_and_receive();
+		if((_temp = measure_temp()) != INT_MIN) { // if a temp value is here...
+			temp = _temp;
+			temp_pending = 1;
 		}
-
-		// if we didn't receive something in the meantime, let some time pass
-		if(pending == 1)
-			_delay_us(100);
-
-		if(pending) {
-			if(yc_poll_transmit(&msg_out) == SUCCESS)
-				pending = 0;
-			else
-				continue;
-		}
-
-		msg_out.length = 0;
-		msg_out.info = 0;
-
-		if(fifo2_read.count < 8 && fifo2_read.count > 0) {
-			if(fifo2_read.count > _count) {
-				_count = fifo2_read.count;
-				_timer = 0;
-			}
-			if(_timer < 50) // ms timeout for aggregating messages
-				continue;
-		}
-
-		while((c = uart_getc_nowait()) != -1 && msg_out.length < 8)
-			msg_out.data[msg_out.length++] = c;
-		if(msg_out.length) {
+		if(!pending && temp_pending) {
+			temp_pending = 0;
+			msg_out.id = temp_id;
+			msg_out.length = 2;
+			msg_out.info = 0;
+			msg_out.data[0] = (uint8_t) (temp >> 8);
+			msg_out.data[1] = (uint8_t) temp;
 			if(yc_transmit(&msg_out) == PENDING)
 				pending = 1;
 		}
@@ -91,6 +165,7 @@ int main() {
 
 ISR(TIMER1_COMPA_vect) {
 	_timer++;
+	_conv_timer++;
 }
 
 #include "fifo.c"
--- src/embedded/control-panel/ControlPanel.h
+++ src/embedded/control-panel/ControlPanel.h
@@ -3,6 +3,7 @@
 
 #define YC_EE_TCP_IN_ID YE(508) // int32
 #define YC_EE_TCP_OUT_ID YE(504) // int32
+#define YC_EE_TEMPSTATUS_ID YE(500) // int32
 
 namespace ControlPanel {
 }
