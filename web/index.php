80cfe96991ab9cd764b8d65b06c7b4d86033007d
David Madl <freeware@abanbytes.eu>
2010-07-28 12:45:15 +0200

    web: synchronize clock via JS every minute

--- web/index.php
+++ web/index.php
@@ -19,7 +19,39 @@ function el(n) {
 	}
 }
 
+function createAjax() {
+	var x = null;
+	try {
+		x = new XMLHttpRequest();
+	} catch(e) {
+		try {
+			x = new ActiveXObject("Microsoft.XMLHTTP");
+		} catch(e) {
+			try {
+				x = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch(e) {
+				x = null;
+			}
+		}
+	}
+	return x;
+}
+
+var ajax_num = 0;
 function clock() {
+	/* synchronize clock every minute */
+	if(cur_date.getSeconds() == 0) {
+		var aj = createAjax();
+		if(aj) {
+			aj.open('GET', 'time.php?x=' + (ajax_num++), true);
+			aj.onreadystatechange = function() {
+				if(x.readyState == 4) {
+					cur_date = eval('new Date(' + aj.responseText + ')');
+				}
+			}
+			aj.send();
+		}
+	}
 	cur_date.setSeconds(cur_date.getSeconds() + 1);
 	el("time").innerHTML = cur_date.toLocaleString();
 	setTimeout('clock()', 1000);
@@ -35,8 +67,6 @@ function clock() {
 $pool->render();
 
 echo "<br /><br />Time: ";
-
-$time->render();
 
 ?>
 <div id="time">
