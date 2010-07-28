cce9b7172df196819a523ef7ac1e5346126bcae2
David Madl <freeware@abanbytes.eu>
2010-07-28 12:36:01 +0200

    web: split off includes

--- web/index.php
+++ web/index.php
@@ -1,20 +1,6 @@
 <?php
 
-require_once('common.inc.php');
-//require_once('led.class.php');
-//require_once('main-power.class.php');
-require_once('pool-control.class.php');
-require_once('time.class.php');
-
-$common = new Common("192.168.1.2", 1111);
-
-//$led = new Led($common, array('canid_read' => 200, 'canid_write' => 1));
-//$mp = new MainPower($common, array('canid_powerstatus' => 400, 'canid_charge' => 2));
-$time = new Time($common, array('canid_time' => 401));
-$pool = new PoolControl($common, array('canid_phstatus' => 402, 'canid_tempstatus' => 403, 'canid_relaystatus' => 201));
-
-$common->handleRequests();
-usleep(100 * 1000);
+require_once('includes.php');
 
 ?><html>
 <head>
