e5f70ee42923856f9be868a9ef47cf7fecf4a2c1
David Madl <freeware@abanbytes.eu>
2010-08-01 18:24:17 +0200

    yaca-timed: fixed read buffer overflowing

--- src/x86/yaca-timed/main.cpp
+++ src/x86/yaca-timed/main.cpp
@@ -6,10 +6,14 @@
 #include <fcntl.h>
 #include <time.h>
 #include <stdint.h>
+#include <sys/types.h>
+#include <sys/time.h>
 
 //#include "config.h"
 #include "network.h"
 #include "../yaca-path.h"
+
+#define CAN_TIME_ID 401
 
 int main(int argc, char **argv) {
 	int sock = 0, flags, pid;
@@ -17,7 +21,9 @@ int main(int argc, char **argv) {
 //	char config_file[1024];
 	Message msg;
 	struct timespec t, d;
+	struct timeval tv;
 	struct tm *tm;
+	fd_set fds;
 	
 	init_yaca_path();
 //	sprintf(config_file, "%s/src/x86/mpmon/conf/mpmon.conf", yaca_path);
@@ -26,16 +32,6 @@ int main(int argc, char **argv) {
 	if((sock = connect_socket("192.168.1.1", 1222)) == -1)
 		return 1;
 
-	if((flags = fcntl(sock, F_GETFD)) == -1) {
-		fprintf(stderr, "fcntl(sock, F_GETFD) failed\n");
-		return 1;
-	}
-	// set O_NONBLOCK on 'sock'
-	if(fcntl(sock, F_SETFD, flags | O_NONBLOCK) == -1) {
-		fprintf(stderr, "fcntl(sock, F_SETFD, flags | O_NONBLOCK) failed\n");
-		return 1;
-	}
-	
 	pid = fork();
 	if(pid < 0) {
 		fprintf(stderr, "fork() failed\n");
@@ -53,8 +49,16 @@ int main(int argc, char **argv) {
 		
 		t.tv_sec++;
 		tm = localtime(&t.tv_sec);
-		write_message(sock, /* TODO */ 401, 8, tm->tm_hour, tm->tm_min, tm->tm_sec, (tm->tm_year + 1900) >> 8, (uint8_t) (tm->tm_year + 1900), tm->tm_mon + 1, tm->tm_mday, tm->tm_isdst);
-//		read(sock, buf, sizeof(buf)); // TODO: check if overflowing buffers cause problems. this is commented out for now, because it takes ages even with O_NONBLOCK
+		write_message(sock, CAN_TIME_ID, 8, tm->tm_hour, tm->tm_min, tm->tm_sec, (tm->tm_year + 1900) >> 8, (uint8_t) (tm->tm_year + 1900), tm->tm_mon + 1, tm->tm_mday, tm->tm_isdst);
+
+		/* even though no data is needed, read to prevent buffers from overflowing and Linux auto-closing the connection */
+		FD_ZERO(&fds);
+		FD_SET(sock, &fds);
+		tv.tv_sec = 0;
+		tv.tv_usec = 0;
+		select(sock + 1, &fds, NULL, NULL, &tv);
+		if(FD_ISSET(sock, &fds))
+			read(sock, buf, sizeof(buf));
 	}
 
 #ifdef _WIN32
