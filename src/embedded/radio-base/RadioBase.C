97b559a9b689c8f709d8f3775a70136543b4cef5
David Madl <freeware@abanbytes.eu>
2010-09-06 14:33:34 +0200

    radio-base: disable RFM12 for now

--- src/embedded/radio-base/RadioBase.C
+++ src/embedded/radio-base/RadioBase.C
@@ -1,11 +1,17 @@
 #include "RadioBase.h"
 #include "RRadioBase.h"
 #include <yaca.h>
+#include <avr/io.h>
 
 uint8_t _n = 0;
 
 void DR(Wtf()) {
 	yc_prepare(406);
 	yc_send(RadioBase, Wtf(_n++));
+}
+
+void init() {
+	DDRB |= (1 << PB1);
+	PORTB |= (1 << PB1); // disable CS of RFM12
 }
 
