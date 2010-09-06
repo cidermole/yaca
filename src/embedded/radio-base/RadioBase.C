be99005618e0aa041413180935dc592f5c378e80
David Madl <freeware@abanbytes.eu>
2010-09-06 13:47:55 +0200

    radio-base: test code added

--- src/embedded/radio-base/RadioBase.C
+++ src/embedded/radio-base/RadioBase.C
@@ -1,2 +1,11 @@
 #include "RadioBase.h"
+#include "RRadioBase.h"
 #include <yaca.h>
+
+uint8_t _n = 0;
+
+void DR(Wtf()) {
+	yc_prepare(406);
+	yc_send(RadioBase, Wtf(_n++));
+}
+
--- src/embedded/radio-base/RadioBase.h
+++ src/embedded/radio-base/RadioBase.h
@@ -2,5 +2,8 @@
 #include <inttypes.h>
 
 namespace RadioBase {
+	void HDS(Wtf(uint8_t c));
+
+	void HDR(Wtf());
 }
 
