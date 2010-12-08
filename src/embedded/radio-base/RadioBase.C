fda47a59f20aadc44058267bbd4dc1c037db286a
David Madl <freeware@abanbytes.eu>
2010-12-08 17:05:39 +0100

    radio-base: create random seed from time hash

--- src/embedded/radio-base/RadioBase.C
+++ src/embedded/radio-base/RadioBase.C
@@ -10,9 +10,15 @@
 #endif
 #include <util/delay.h>
 #include <string.h>
+#include "/home/david/Info/yaca-aeskey.h"
+// yaca-aeskey.h only contains the following line: const uint8_t _flash_aes_key[16] PROGMEM = {...};
+
+#define HASH_PRIME 12211009UL // a prime < 2^24
 
 uint16_t voltage;
 int16_t temperature;
+uint32_t random_seed = 0;
+extern uint8_t aes_key[16];
 
 void DR(TempStatus()) {
 	yc_prepare_ee(YC_EE_TEMPSTATUS_ID);
@@ -21,13 +27,36 @@ void DR(TempStatus()) {
 	RFM12_INT_on();
 }
 
+void hash_step(uint32_t *hash_val, uint8_t data) {
+	*hash_val = (((*hash_val) << 8) + ((uint32_t) data)) % HASH_PRIME;
+}
+
 void DM(Time(uint8_t hour, uint8_t min, uint8_t sec, uint16_t year, uint8_t month, uint8_t day, uint8_t flags)) {
+	uint32_t hash_val = 0;
+
+	if(random_seed == 0) {
+		hash_step(&hash_val, hour);
+		hash_step(&hash_val, min);
+		hash_step(&hash_val, sec);
+		hash_step(&hash_val, (uint8_t) year);
+		hash_step(&hash_val, (uint8_t) (year >> 8));
+		hash_step(&hash_val, month);
+		hash_step(&hash_val, day);
+	}
 }
 
 int main() {
 	RadioMessage rmsg;
+
 	sei();
+
+	while(random_seed == 0)
+		yc_dispatch_auto();
+
+	srandom(random_seed); // initialize non-standard avr-libc 32-bit random number generator
+
 	RFM12_include(); // dummy call to librfm12 to make linker happy
+	aes_key_expand(aes_key, _flash_aes_key, AES_KEY_FLASH);
 	radio_init(1);
 
 	while(1) {
