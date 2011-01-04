d2bbdf348aa4424a7ca452a20aaaa15a94a400db
Yaca user <yaca@nas.lan>
2011-01-05 00:08:19 +0100

    web: update_f.php updated with NAS IP

--- web/update_f.php
+++ web/update_f.php
@@ -3,5 +3,5 @@
 require_once('includes.php');
 
 ?><script>
-parent.cont.onl('<?php echo str_replace("\n", "\\\n", str_replace("'", "\\'", file_get_contents("http://192.168.1.2/yaca/update.php"))); ?>');
+parent.cont.onl('<?php echo str_replace("\n", "\\\n", str_replace("'", "\\'", file_get_contents("http://192.168.1.3/yaca/update.php"))); ?>');
 </script>
