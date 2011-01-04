81c959ce60bf98ec932ca4a6a36292208a84f681
David Madl <freeware@abanbytes.eu>
2011-01-04 21:30:07 +0100

    web: includes.php: yaca-cached moved to 192.168.1.3 (NAS)

--- web/includes.php
+++ web/includes.php
@@ -7,7 +7,7 @@ require_once('pool-control.class.php');
 require_once('time.class.php');
 require_once('temp.class.php');
 
-$common = new Common("192.168.1.2", 1111);
+$common = new Common("192.168.1.3", 1111);
 
 //$led = new Led($common, array('canid_read' => 200, 'canid_write' => 1));
 //$mp = new MainPower($common, array('canid_powerstatus' => 400, 'canid_charge' => 2));
