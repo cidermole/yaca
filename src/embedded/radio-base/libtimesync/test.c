a3faa82446ee9a1833af3a9e5eb36299f53c6faf
David Madl <freeware@abanbytes.eu>
2011-02-22 19:30:50 +0100

    libtimesync: test.c made work like original

--- src/embedded/radio-base/libtimesync/test.c
+++ src/embedded/radio-base/libtimesync/test.c
@@ -10,7 +10,7 @@ double rnd() {
 #define JITTER 30
 
 int main(int argc, char **argv) {
-	int real_time, next_stamp, next_stamp_real, fb, armed = 0;
+	int real_time, next_stamp, next_stamp_real, fb;
 	double val = 0, input = 0, soll, soll2; // soll: time feedback with jitter
 	double time_counter = 0, corrected_counter = 0;
 	double delayed_time_counter;
@@ -27,16 +27,9 @@ int main(int argc, char **argv) {
 	next_stamp_real = 0;
 	for(real_time = 0; real_time < max; real_time++, time_counter += DEVIATION, corrected_counter += DEVIATION) {
 		if(real_time == next_stamp) {
-			delayed_time_counter = time_counter;
+			ts_slot(((int) time_counter) % 60000, ((int) corrected_counter) % 60000, next_stamp_real % 60000);
 			next_stamp_real += 1000;
 			next_stamp = next_stamp_real + (JITTER / 2) - rnd() * JITTER;
-			armed = 1;
-		}
-		if(((((int)corrected_counter) >= next_stamp_real - 503 && ((int)corrected_counter) <= next_stamp_real - 497) || (real_time < 120000 && real_time == next_stamp_real - 500)) && armed) {
-//		if(real_time == next_stamp_real - 500) {
-			ts_slot(((int) delayed_time_counter) % 60000, ((int) corrected_counter) % 60000, ((int) (next_stamp_real - 500)) % 60000);
-			printf("w00t cc: %d real: %d\n", (int) corrected_counter, real_time);
-			armed = 0;
 		}
 
 		fb = ts_tick(((int) time_counter) % 60000);
