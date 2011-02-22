12e2c52ca49e20fd09d18e63c58dd13b26eedc17
David Madl <freeware@abanbytes.eu>
2011-02-22 10:38:42 +0100

    libtimesync: test.c imported from testing repo

--- src/embedded/radio-base/libtimesync/test.c
+++ src/embedded/radio-base/libtimesync/test.c
@@ -1,84 +1,48 @@
-#include "timesync.h"
 #include <stdio.h>
-#include <stdint.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include "timesync.h"
 
-#define JITTER_MS 30 // +/-
-#define JITTER_SAFE_MAX 2000 // safety margin which jitter must never exceed
-
-double _mscounter;
-const double ms_increment = 0.99;
-
-extern int16_t drift_sum;
-
-uint16_t mscounter() {
-	return _mscounter;
+double rnd() {
+	return ((double) rand()) / RAND_MAX;
 }
 
-int main() {
-	uint16_t t, nr;
-	int8_t tick;
-	int i;
+#define JITTER 30
 
-	srand(2);
+int main() {
+	int real_time, next_stamp, next_stamp_real, fb;
+	double val = 0, input = 0, soll, soll2; // soll: time feedback with jitter
+	double time_counter = 0, corrected_counter = 0;
+	double delayed_time_counter;
+	double DEVIATION = 0.96;
+
+	srand(0);
 	ts_init();
 
-	_mscounter = JITTER_MS;
-
-	for(i = 0; i < 100; i++) {
-		nr = 10000 + (rand() % (2 * JITTER_MS)) - JITTER_MS;
-		for(t = 0; t < 60000; t++) {
-			if(t % 1000 == 0)
-				//fprintf(stderr, "T M D: %d %d %d\n", (int) t, (int) mscounter(), (int) (t - mscounter()));
-				//fprintf(stderr, "%d;%d\n", i * 60000 + ((int) t), i * 60000 + ((int) mscounter()) + (t == 0 && mscounter() > 20000 ? -60000 : 0));
-				fprintf(stderr, "%d;%d;%d\n", i * 60 + ((int) t / 1000), (int) ((mscounter() - t) > 30000 || (mscounter() - t) < -30000) ? 0 : (mscounter() - t), (int) drift_sum);
-			tick = 0;
-			if(t == nr) {
-				//printf("ts_slot(%d)\n", (int) mscounter());
-				ts_slot(mscounter());
-				nr = ((((nr + JITTER_SAFE_MAX) / 10000) + 1) * 10000  + (rand() % (2 * JITTER_MS)) - JITTER_MS) % 60000;
-			} else {
-				tick = ts_tick(mscounter());
-			}
-
-			if(tick == 1)
-				_mscounter += ms_increment;
-			if(tick != -1)
-				_mscounter += ms_increment;
-			if(mscounter() >= 60000)
-				_mscounter -= 60000;
+	next_stamp = JITTER - rnd() * JITTER;
+	next_stamp_real = 0;
+	for(real_time = 0; real_time < 2000 * 1000; real_time++, time_counter += DEVIATION, corrected_counter += DEVIATION) {
+		if(real_time == next_stamp) {
+			delayed_time_counter = time_counter;
+			next_stamp_real += 1000;
+			next_stamp = next_stamp_real + (JITTER / 2) - rnd() * JITTER;
 		}
+		if(real_time == next_stamp_real - 500)
+			ts_slot(((int) delayed_time_counter) % 60000, ((int) corrected_counter) % 60000, ((int) (next_stamp_real - 500)) % 60000);
+
+		fb = ts_tick(((int) time_counter) % 60000);
+		if(fb == 1)
+			corrected_counter += DEVIATION;
+		else if(fb == -1)
+			corrected_counter -= DEVIATION;
+
+		printf("%d;%d;%d\n", real_time, /*(int) soll*/ real_time, (int) corrected_counter);
+		fprintf(stderr, "%d;%d\n", real_time, (int) corrected_counter - real_time);
+
+/*		if(real_time < 1000000)
+			DEVIATION += 0.2 / 10000000;*/
 	}
 
 	return 0;
 }
-
-
-/*
-extern uint16_t tick_dist, next_tick_ms;
-extern int16_t tick_rem;
-extern int8_t tick_sign;
-
-void dump() {
-	printf("tick_dist: %d\n", (int) tick_dist);
-	printf("tick_sign: %d\n", (int) tick_sign);
-	printf("tick_rem: %d\n", (int) tick_rem);
-	printf("next_tick_ms: %d\n", (int) next_tick_ms);
-	printf("\n");
-}
-
-
-int main() {
-	printf("ts_init()\n");
-	ts_init();
-	dump();
-	printf("ts_slot()\n");
-	ts_slot(14327);
-	dump();
-	printf("ts_tick()\n");
-	ts_tick(14465);
-	dump();
-	return 0;
-}
-*/
 
