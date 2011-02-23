3520faa4b1b0c134ae93f6d3c77fc3b73ed6cd14
David Madl <freeware@abanbytes.eu>
2011-02-23 13:08:50 +0100

    radio-base: begin rewrite for libtimesync

--- src/embedded/radio-base/RadioBase.C
+++ src/embedded/radio-base/RadioBase.C
@@ -2,6 +2,7 @@
 #include "RRadioBase.h"
 #include <yaca.h>
 #include "libradio/radio.h"
+#include "libtimesync/timesync.h"
 #include "librfm12/rfm12.h"
 #include <stdint.h>
 #include <avr/io.h>
@@ -15,7 +16,9 @@
 
 uint16_t voltage;
 int16_t temperature;
-volatile uint16_t ms_timer_count = 0;
+volatile int32_t timer_local = 0, timer_corr = 0;
+volatile uint8_t skip_corr = 0;
+int32_t old_time = 0;
 
 void DR(TempStatus()) {
 	yc_prepare_ee(YC_EE_TEMPSTATUS_ID);
@@ -27,62 +30,55 @@ void DR(TempStatus()) {
 extern "C" {
 	uint16_t ms_timer();
 }
+int32_t ms_timer_local();
+int32_t ms_timer_corr();
 
 void debug_tx(volatile uint8_t *p) {
 	yc_send(RadioBase, Debug(p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7]));
 }
 
 #define MAX_DEVIATION_MS 200
-#define SYNCPOINT_SECS 30
-#define MAX_OSCCAL_DEVIATION 10
-//#define FINE_SYNC_MS 20
 
 void DM(Time(uint8_t hour, uint8_t min, uint8_t sec, uint16_t year, uint8_t month, uint8_t day, uint8_t flags)) {
-	uint16_t timer_count = ms_timer(); // % 1000
-	uint16_t timer_sub = timer_count % 1000;
-	static uint8_t cal = 0;
-	uint8_t debug_msg[8];
+	//uint8_t debug_msg[8];
+	int32_t reported_time = 3600000UL * hour + 60000UL * min + 1000UL * sec;
+	static uint8_t old_day = 0;
 
-	// XXX: let's hope OSCCAL will never be 0 (static uint8_t cal = OSCCAL doesn't make compiler happy)
-	if(cal == 0)
-		cal = OSCCAL;
+	// TODO: coarse sync
 
-	if(sec == SYNCPOINT_SECS && (timer_count / 1000 != SYNCPOINT_SECS || timer_sub < (500 - MAX_DEVIATION_MS) || timer_sub > (500 + MAX_DEVIATION_MS))) {
+	if(old_day != day) {
+		// daily counter reset at midnight
 		cli();
-		ms_timer_count = SYNCPOINT_SECS * 1000 + 500;
+		timer_local = 0;
+		timer_corr = 0;
+		old_time = 0;
 		sei();
-
-		// debugging
-		yc_prepare(798);
-		//debug_msg[0] = 0x01;
-		debug_msg[0] = hour;
-		debug_msg[1] = min;
-		debug_msg[2] = sec;
-		debug_tx(debug_msg);
+		ts_tick(0, 1); // reset tick
 	} else {
-		// fine sync
-		if(timer_sub > 500 && OSCCAL > cal - MAX_OSCCAL_DEVIATION) // too fast?
-			OSCCAL--;
-		else if(timer_sub < 500 && OSCCAL < cal + MAX_OSCCAL_DEVIATION) // too slow?
-			OSCCAL++;
-
-		// debugging
-		if((timer_sub > 500 && OSCCAL == cal - MAX_OSCCAL_DEVIATION) || (timer_sub < 500 && OSCCAL == cal + MAX_OSCCAL_DEVIATION)) {
-			yc_prepare(797);
-			debug_msg[0] = 0x02;
-			debug_msg[1] = timer_sub > 500;
-			debug_tx(debug_msg);
-		}
+		ts_slot(ms_timer_local(), ms_timer_corr(), reported_time);
 	}
 }
 
 
 // for libradio-master
 uint16_t ms_timer() {
-	uint16_t t;
+	return ms_timer_corr() % 60000;
+}
+
+int32_t ms_timer_local() {
+	int32_t t;
 	uint8_t sr = SREG;
 	cli();
-	t = ms_timer_count;
+	t = timer_local;
+	SREG = sr;
+	return t;
+}
+
+int32_t ms_timer_corr() {
+	int32_t t;
+	uint8_t sr = SREG;
+	cli();
+	t = timer_corr;
 	SREG = sr;
 	return t;
 }
@@ -96,10 +92,12 @@ void enter_bootloader_hook() {
 
 int main() {
 	RadioMessage rmsg;
+	int8_t fb;
 
 	TCCR2 = (1 << CS21) | (1 << WGM21); // CTC mode, prescaler = 8
 	OCR2 = (uint16_t)((125UL * F_CPU) / 1000000UL); // 8 * 125 = 1000 ticks/s.
 	TIMSK = (1 << OCIE2); // enable OC match interrupt
+	ts_init();
 
 	sei();
 
@@ -115,13 +113,30 @@ int main() {
 			yc_status(TempStatus);
 		}
 		yc_dispatch_auto();
+
+		if(ms_timer_local() != old_time) {
+			fb = ts_tick(ms_timer(), 0);
+			if(fb == 1) {
+				cli();
+				timer_corr++;
+				sei();
+			} else if(fb == -1) {
+				cli();
+				skip_corr = 1;
+				sei();
+			}
+			old_time++;
+		}
+		yc_dispatch_auto();
 	}
 	return 0;
 }
 
-//TODO: synchronize ms_timer with CAN bus time
 ISR(TIMER2_COMP_vect) {
-	if(++ms_timer_count == 60000)
-		ms_timer_count = 0;
+	if(skip_corr)
+		skip_corr = 0;
+	else
+		timer_corr++;
+	timer_local++;
 }
 
