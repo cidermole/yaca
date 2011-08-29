f9b225981921cfd8ff60b916692fdd989130ee55
David Madl <david.madl@abanbytes.eu>
2011-08-29 22:19:24 +0200

    embedded: added EntryBox

---
+++ src/embedded/entrybox/EntryBox.C
@@ -0,0 +1,2 @@
+#include "EntryBox.h"
+
---
+++ src/embedded/entrybox/EntryBox.h
@@ -0,0 +1,14 @@
+#include "Messages.h"
+#include <stdint.h>
+
+//#define YC_EE_COUNT_ID YE(508) // int32
+
+namespace CarCounter {
+/*
+	void HDS(Count(uint32_t count));
+	void HDR(Count());
+
+	void HDM(SetCount(uint32_t count));
+*/
+}
+
