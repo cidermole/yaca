6d1a65667bc81464e07f241e9e792a54e6118af8
David Madl <david.madl@abanbytes.eu>
2011-09-04 17:57:12 +0200

    entrybox: coulomb (aka joule) counter, first draft

--- src/embedded/entrybox/EntryBox.C
+++ src/embedded/entrybox/EntryBox.C
@@ -1,7 +1,13 @@
 #include "EntryBox.h"
+#include "REntryBox.h"
+#include <stdint.h>
 #include <yaca.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+
+#ifndef INT32_MAX
+#define INT32_MAX 0x7fffffffL
+#endif
 
 /*
 
@@ -9,24 +15,65 @@ PB1 (OC1A): charge pump for opamps
 PD5: photo camera (N-channel MOSFET gate)
 PD6: dummy load (N-channel MOSFET gate)
 
+ADC0: IBAT (battery current), 6.5 mA resolution
+ADC1: VBAT (battery voltage), 15 mV resolution
+ADC2: ISOL (solar [charge] current), 6.5 mA resolution
+
+  |
+  v IBAT
++ |
+-----  12 V lead-acid battery
+ ---
+  |
+
+
+
+coulomb counter
+---------------
+basic resolution: U = 15 mV, I = 6.5 mA, P = 97.5 uW (microwatt)
+
+ADC low-pass filter tau = 2 ms -> 10 ms changes relevant -> 100 Hz samplerate
+
+12 V 7 Ah capacity fits into 32 bit
+1 year's worth of microjoules don't fit
+
+~ 10256 P units equal 1 Watt
+1 year's worth of Joules fit. -> energy counting in Joules
+
+
 */
+
+#define ADC_IBAT 0
+#define ADC_VBAT 1
+#define ADC_ISOL 2
+
+#define ADMUX_REF ((1 << REFS1) | (1 << REFS0)) // internal AREF = 2.56 V with external cap
+
+#define JOULE_100_PUNITS 1025641L
+#define JOULE_BATTERY_FULL (12 * 7 * 3600UL)
+#define MAXIMUM_POWER_OUTPUT 42 // 42: 14 V * 3 A, the expected maximum output power
 
 void enter_bootloader_hook() {
 	TCCR1A = 0;
 	TCCR1B = 0;
-	//TIMSK = 0;
+	TIMSK = 0;
 	cli();
 	yc_bld_reset();
 }
 
-void init_timer() {
+void init_timers() {
 	OCR1A = 100; // 2 MHz / 100 = 20 kHz toggle -> 10 kHz frequency generator
 	TCCR1A = (1 << COM1A0); // toggle OC1A on compare match
 	TCCR1B = (1 << WGM12) | (1 << CS10); // CTC mode, top = OCR1A, prescaler = 1
+
+	OCR2 = 156; // 2 MHz / (156 * 128) = ~ 100 Hz
+	TCCR2 = (1 << WGM21) | (1 << CS22) | (1 << CS20); // CTC mode, prescaler = 128
+
+	TIMSK = (1 << OCIE2); // enable Timer2 compare match interrupt
 }
 
 void init_adc() {
-	ADMUX = (1 << REFS1) | (1 << REFS0); // Vref = internal 2.56 V with external cap
+	ADMUX = ADMUX_REF; // Vref = internal 2.56 V with external cap
 	ADCSRA = (1 << ADEN) | (1 << ADPS2); // ADC prescaler = 16 -> 2 MHz / 16 = 125 kHz
 }
 
@@ -35,13 +82,109 @@ void init_ports() {
 	DDRD |= (1 << PD6) | (1 << PD5); // dummy, camera
 }
 
+int16_t adc_convert(uint8_t channel) {
+	ADMUX = (channel & 0x07) | ADMUX_REF; // select channel (+ keep reference)
+	ADCSRA |= (1 << ADSC) | (1 << ADIF); // start conversion, clear int flag
+	while(!(ADCSRA & (1 << ADIF))); // wait for conversion
+	return ADCW;
+}
+
+int16_t ibat_offset = 512;
+int16_t isol, vbat, ibat;
+int32_t sum_punit = 0;
+uint32_t sum_punit_solar = 0;
+uint32_t joule_battery = JOULE_BATTERY_FULL;
+uint32_t joule_solar = 0;
+
+void time_tick();
+
+void conversion_tick() {
+	static uint8_t time = 0;
+	int32_t power;
+	/*int16_t*/ vbat = adc_convert(ADC_VBAT);
+	/*int16_t*/ ibat = adc_convert(ADC_IBAT) - ibat_offset;
+
+	power = ((int32_t) vbat) * ibat;
+	sum_punit += power;
+	sum_punit_solar += (uint32_t) ((int32_t) vbat) * isol;
+
+	isol = adc_convert(ADC_ISOL);
+
+	if(++time == 100) {
+		time_tick();
+		time = 0;
+	}
+}
+
+void calibrate() {
+	// TODO: turn off all power consumers, restore afterwards
+
+	ibat_offset = adc_convert(ADC_IBAT); // measure zero current offset
+}
+
+void time_tick() {
+	static uint8_t tick = 0;
+	// update joule counter and power
+	int16_t djoule = sum_punit / JOULE_100_PUNITS;
+	int16_t djoule_solar = sum_punit_solar / JOULE_100_PUNITS;
+
+	// one-time init
+	if(tick == 0) {
+		calibrate();
+		djoule = 0;
+		djoule_solar = 0;
+		sum_punit = 0;
+		sum_punit_solar = 0;
+	}
+
+	sum_punit -= ((int32_t) djoule) * JOULE_100_PUNITS;
+	sum_punit_solar -= ((uint32_t) djoule_solar) * JOULE_100_PUNITS;
+	joule_battery += djoule;
+	joule_solar += djoule_solar;
+
+	if(joule_battery >= INT32_MAX - MAXIMUM_POWER_OUTPUT)
+		joule_battery = 0; // this is probably an unsigned underrun - clear capacity
+	else if(joule_battery > JOULE_BATTERY_FULL)
+		joule_battery = JOULE_BATTERY_FULL; // battery can never be charged over its full capacity
+
+	tick++;
+	if(tick % 2 == 0) {
+		yc_status(JouleStatus);
+		tick = 2;
+	} else
+		yc_status(PowerStatus);
+}
+
+void DR(JouleStatus()) {
+	yc_prepare_ee(YC_EE_JOULESTATUS_ID);
+	yc_send(EntryBox, JouleStatus(joule_battery, joule_solar));
+}
+
+void DR(PowerStatus()) {
+	yc_prepare_ee(YC_EE_POWERSTATUS_ID);
+	yc_send(EntryBox, PowerStatus(sum_punit, sum_punit_solar));
+}
+
+volatile uint8_t start_conversion = 0;
+
 int main() {
 	init_ports();
-	init_timer();
+	init_timers();
 	init_adc();
 	sei();
 	while(1) {
+		if(start_conversion) {
+			cli();
+			start_conversion--;
+			sei();
+			conversion_tick();
+		}
 		yc_dispatch_auto();
 	}
+}
+
+// 100 Hz interrupt
+ISR(TIMER2_COMP_vect) {
+	start_conversion++;
 }
 
--- src/embedded/entrybox/EntryBox.h
+++ src/embedded/entrybox/EntryBox.h
@@ -1,14 +1,15 @@
 #include "Messages.h"
 #include <stdint.h>
 
-//#define YC_EE_COUNT_ID YE(508) // int32
+#define YC_EE_JOULESTATUS_ID YE(508) // int32
+#define YC_EE_POWERSTATUS_ID YE(504) // int32
 
-namespace CarCounter {
-/*
-	void HDS(Count(uint32_t count));
-	void HDR(Count());
+namespace EntryBox {
+	void HDS(JouleStatus(uint32_t joule_battery, uint32_t joule_solar));
+	void HDR(JouleStatus());
+	void HDS(PowerStatus(int32_t spu_battery, uint32_t spu_solar));
+	void HDR(PowerStatus());
 
-	void HDM(SetCount(uint32_t count));
-*/
+	//void HDM(SetCount(uint32_t count));
 }
 
