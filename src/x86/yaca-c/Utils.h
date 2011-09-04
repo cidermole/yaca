22de52c7345901b738a42c6c96813963906487be
David Madl <david.madl@abanbytes.eu>
2011-09-04 15:41:40 +0200

    yaca-c Utils.h: fixed error output

--- src/x86/yaca-c/Utils.h
+++ src/x86/yaca-c/Utils.h
@@ -2,10 +2,18 @@
 #define _UTILS_H_
 
 #include <string>
+#include <string.h>
 #include <list>
 
 #define throwable() string thr("")
-#define throw_error(s) throw (thr + s).c_str()
+//#define throw_error(s) throw (thr + s).c_str()
+#define throw_error(s) \
+	do { \
+		char __errbuf[100*1024]; \
+		strncpy(__errbuf, (thr + s).c_str(), 100*1024-1); \
+		__errbuf[100*1024-1] = '\0'; \
+		throw __errbuf; \
+	} while(0)
 #define ES thr
 
 using namespace std;
